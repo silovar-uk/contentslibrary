13beeac0a703b91897af69c38dc98c777a4e5cd5
silovar-uk <46244124+silovar-uk@users.noreply.github.com>
2026-09-14 23:45:13 +0900

    表紙あつめをアプリ初期化へ接続する

--- public/app.js
+++ public/app.js
@@ -18,6 +18,7 @@ import { initDetail, openDetail } from "./views/detail.js";
 import { initDetailTopNotes } from "./views/detail-top-notes.js";
 import { initAmazonTitleSearch } from "./views/amazon-title-search.js";
 import { initCoverResolution } from "./views/cover-resolution.js";
+import { initCoverHunt } from "./views/cover-hunt.js";
 import { initWordMemos } from "./views/word-memos.js";
 import { initWordMemoBulkSelection } from "./views/word-memo-bulk-selection.js";
 import { initReviewSummary } from "./views/review-summary.js";
@@ -132,6 +133,7 @@ async function init() {
     initWorkMediaUrl();
     initDialogs();
     initMobileSearch();
+    initCoverHunt();
     initCommandPalette();
     initBulkAdd();
     initBulkJsonAdd();
